9bf01ebd6413043173d41c34b834387c34a601b9
Ian Nastajus <ian.nastajus@gmail.com>
2018-04-24 15:30:40 -0400

    accessing various parts of the page

--- index.js
+++ index.js
@@ -13,7 +13,21 @@ const options = {
 rp(options)
 	.then(function (data) {
 		// REQUEST SUCCEEDED: DO SOMETHING
-		console.log(data);
+		//console.log(data);
+
+		//data('.large').text()
+		// New York
+
+		//var test = data('span[style="color:brown"]').text();
+		//var test = data('span').text(); //result: .com×××HTML5HTMLHTML5HTMLHTMLHTMLHTMLHTML Tutorial××
+		//var test = data('h3');
+		//var test = data('span[style="color:brown"]').html();    //null
+		//var test = data('span[style="color:brown"]');    //1 item
+		var test = data('.w3-example').html();
+		console.log(test);
+
+
+
 	})
 	.catch(function (err) {
 		// REQUEST FAILED: ERROR OF SOME KIND
